96949937b6ebf1989305ff455ea45c863534d55a
Valentina Valverde <vvgreenville@gmail.com>
2023-10-29 15:35:16 -0400

    Finished the tutorial.

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -1,101 +1,134 @@
 import { json } from "@shopify/remix-oxygen";
 import { useLoaderData } from "@remix-run/react";
-import { Image, Money, ShopPayButton } from "@shopify/hydrogen";
+import { Image, Money, ShopPayButton, CartForm } from "@shopify/hydrogen";
 import ProductOptions from "~/components/ProductOptions";
 
 export async function loader({ params, context, request }) {
-    const { handle } = params;
-    const searchParams = new URL(request.url).searchParams;
-    const selectedOptions = [];
+  const { handle } = params;
+  const searchParams = new URL(request.url).searchParams;
+  const selectedOptions = [];
 
-    searchParams.forEach((value, name) => {
-        selectedOptions.push({ name, value });
-    })
+  searchParams.forEach((value, name) => {
+    selectedOptions.push({ name, value });
+  })
 
-    const { shop, product } = await
-        context.storefront.query(PRODUCT_QUERY, {
-            variables: {
-                handle,
-                selectedOptions,
-            }
-        });
+  const { shop, product } = await
+    context.storefront.query(PRODUCT_QUERY, {
+      variables: {
+        handle,
+        selectedOptions,
+      }
+    });
 
-    if (!product?.id) {
-        throw new Response(null, { status: 404 });
-    }
+  if (!product?.id) {
+    throw new Response(null, { status: 404 });
+  }
 
-    // Set a default variant so you always have an "orderable" product selected
-    const selectedVariant =
-        product.selectedVariant ?? product?.variants?.nodes[0];
+  // Set a default variant so you always have an "orderable" product selected
+  const selectedVariant =
+    product.selectedVariant ?? product?.variants?.nodes[0];
 
 
-    return json({
-        shop,
-        product,
-        selectedVariant,
-    })
+  return json({
+    shop,
+    product,
+    selectedVariant,
+  })
 }
 //end loader
 
 
 const seo = ({ data }) => ({
-    title: data?.product?.title,
-    description: data?.product?.description.substr(0, 154),
+  title: data?.product?.title,
+  description: data?.product?.description.substr(0, 154),
 });
 
 export const handle = {
-    seo,
+  seo,
 };
 
 
 export default function ProductHandle() {
-    const { selectedVariant, product, shop } = useLoaderData();
-    console.log(product)
+  const { selectedVariant, product, shop } = useLoaderData();
+  console.log(product)
 
-    return (
-        <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
-            <div className="grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
-                <div className="grid md:grid-flow-row  md:p-0 md:overflow-x-hidden md:grid-cols-2 md:w-full lg:col-span-2">
-                    <div className="md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded">
-                        <Image
-                            className={`w-full h-full aspect-square object-cover`}
-                            data={product.selectedVariant?.image || product.featuredImage}
-                        />
-                    </div>
-                </div>
-                <div className="md:sticky md:mx-auto max-w-xl md:max-w-[24rem] grid gap-2 p-0 md:p-6 md:px-0 top-[6rem] lg:top-[8rem] xl:top-[10rem]">
-                    <div className="grid gap-2">
-                        <h1 className="text-4xl font-bold leading-10 whitespace-normal">
-                            {product.title}
-                        </h1>
-                        <span className="max-w-prose whitespace-pre-wrap inherit text-copy opacity-50 font-medium">
-                            {product.vendor}
-                        </span>
-                    </div>
-                    <ProductOptions
-                        options={product.options}
-                        selectedVariant={selectedVariant}
-                    />
-                    <Money
-                        withoutTrailingZeros
-                        data={selectedVariant.price}
-                        className="text-xl font-semibold mb-2"
-                    />
-                    {selectedVariant.availableForSale && (
-                        <ShopPayButton
-                            storeDomain={shop.primaryDomain.url}
-                            variantIds={[selectedVariant?.id]}
-                            width={'400px'}
-                        />
-                    )}
-                    <div
-                        className="prose border-t border-gray-200 pt-6 text-black text-md"
-                        dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
-                    />
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
+      <div className="grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid md:grid-flow-row  md:p-0 md:overflow-x-hidden md:grid-cols-2 md:w-full lg:col-span-2">
+          <div className="md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded">
+            <Image
+              className={`w-full h-full aspect-square object-cover`}
+              data={product.selectedVariant?.image || product.featuredImage}
+            />
+          </div>
+        </div>
+        <div className="md:sticky md:mx-auto max-w-xl md:max-w-[24rem] grid gap-2 p-0 md:p-6 md:px-0 top-[6rem] lg:top-[8rem] xl:top-[10rem]">
+          <div className="grid gap-2">
+            <h1 className="text-4xl font-bold leading-10 whitespace-normal">
+              {product.title}
+            </h1>
+            <span className="max-w-prose whitespace-pre-wrap inherit text-copy opacity-50 font-medium">
+              {product.vendor}
+            </span>
+          </div>
+          <ProductOptions
+            options={product.options}
+            selectedVariant={selectedVariant}
+          />
+          <Money
+            withoutTrailingZeros
+            data={selectedVariant.price}
+            className="text-xl font-semibold mb-2"
+          />
+          {selectedVariant.availableForSale && (
+            <ShopPayButton
+              storeDomain={shop.primaryDomain.url}
+              variantIds={[selectedVariant?.id]}
+              width={'400px'}
+            />
+          )}
+          <CartForm
+            route="/cart"
+            inputs={{
+              lines: [
+                {
+                  merchandiseId: selectedVariant.id,
+                },
+              ],
+            }}
+            action={CartForm.ACTIONS.LinesAdd}
+          >
+            {(fetcher) => (
+              <>
+                <button
+                  type="submit"
+                  onClick={() => {
+                    window.location.href = window.location.href + '#cart-aside';
+                  }}
+                  disabled={
+                    !selectedVariant.availableForSale ??
+                    fetcher.state !== 'idle'
+                  }
+                  className="border border-black rounded-sm w-full px-4 py-2 text-white bg-black uppercase hover:bg-white hover:text-black transition-colors duration-150"
+                >
+                  {selectedVariant?.availableForSale
+                    ? 'Add to cart'
+                    : 'Sold out'}
+                </button>
+              </>
+            )}
+          </CartForm>
+
+
+          <div
+            className="prose border-t border-gray-200 pt-6 text-black text-md"
+            dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
+          />
+        </div>
+      </div>
+    </section>
+  )
 };
 
 
